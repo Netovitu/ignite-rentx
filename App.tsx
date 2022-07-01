15b161253b1db22f7ab5c5f5006803bcf82e037e
Neto <netovitu@gmail.com>
2022-07-01 10:22:52 -0300

    feat: 🎸 add splashscreen on loading app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,73 @@
 //import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+//import 'react-native-gesture-handler';
+import React, { useCallback, useEffect, useState} from 'react';
+import { View } from 'react-native';
+
 
 //import * as SplashScreen from 'expo-splash-screen';
 //import * as Font from 'expo-font';
 import { ThemeProvider } from 'styled-components';
+import { CarDetails } from './src/screens/CarDetails';
 
-//import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
-//import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
 
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
 import { Home } from './src/screens/Home';
 import theme from './src/screens/styles/theme';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare(){
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium,
+          Archivo_400Regular,
+          Archivo_500Medium,
+          Archivo_600SemiBold
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback( async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if(!appIsReady) {
+    return null;
+  }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
+    <View
+      onLayout={onLayoutRootView}
+      style={{flex: 1}}
+    >
+      <ThemeProvider theme={theme}>
+        <CarDetails />
+      </ThemeProvider>
+    </View>
   )
 }
 
--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,17 +1,52 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
-  Header
+  Header,
+  CarImages,
+  Content,
+  Details, 
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About
 } from './styles';
 
 export function CarDetails(){
   return (
     <Container>
       <Header>
-        <BackButton onPress={{} => {}}/>
+        <BackButton 
+          onPress={() => { } } 
+          color={''}
+        />
       </Header>
+
+      <CarImages>
+        <ImageSlider imagesUrl={['https://w7.pngwing.com/pngs/787/25/png-transparent-lightning-mcqueen-lightning-mcqueen-cars-wikia-toy-pixar-cars-3-game-car-transport-thumbnail.png']}/>
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+        <About>
+          Este é uma automóvel desportivo. Surgio o lendário touro de lide indultado,
+          na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
  }
